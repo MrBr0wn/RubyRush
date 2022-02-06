0e9b138ef4cd28e2d8661e032fd8ce12a1df621f
Mr. Brown <gitcherrypick.work@gmail.com>
2022-02-06 18:06:57 +0500

    RSpec training

--- 134_tests_hashtags.rb
+++ 134_tests_hashtags.rb
@@ -1,12 +1,27 @@
 # frozen_string_literal: true
 
-def hashtags(str)
-  regexp_tags = Regexp.new("\#[[:word:]-]+", 'i')
-  str.scan(regexp_tags)
+# Search hashtags in input string
+class HashTag
+  attr_reader :any_tags
+
+  def initialize
+    @any_tags = false
+  end
+
+  def hashtags(str)
+    regexp_tags = Regexp.new("\#[[:word:]-]+", 'i')
+    tags = str.scan(regexp_tags)
+    @any_tags = true if tags.size.positive?
+    tags
+  end
+
+  def has_any_hashtag?
+    @any_tags
+  end
 end
 
 # str = $stdin.gets.chomp
 str = 'Lorem ipsum dolor sit amet, consectetur #anime #аниме sit amet,
 consectetur #fyp #otaku #weeb adipisicing elit, sed do eiusmod
 tempor #fmab #fullmetalalchemist'
-hashtags(str)
+HashTag.new.hashtags(str)
--- 134_tests_hashtags_spec.rb
+++ 134_tests_hashtags_spec.rb
@@ -3,47 +3,85 @@
 require 'rspec'
 require_relative '134_tests_hashtags'
 
-describe 'Seahch hashtags' do
-  string = 'Lorem ipsum dolor sit amet, consectetur #anime #аниме sit amet,
-  consectetur #fyp! #otaku #weeb? adipisicing elit, sed do eiusmod
-  tempor #f-mab #fullmetal_alchemist'
-  simple_string = 'Lorem ipsum dolor sit amet'
-  one_tag_string = 'Lorem ipsum dolor sit amet, consectetur #anime'
-  rus_tag_string = 'Lorem ipsum dolor sit amet, consectetur #аниме'
-  snake_case_tag_string = 'Lorem ipsum adipisicing elit, #fullmetal_alchemist'
-  minus_tag_string = 'Lorem ipsum dolor sit amet, consectetur #f-mab'
-  question_tag_string = 'sit amet, consectetur #weeb?'
-  exclamation_tag_string = 'consectetur sit amet, consectetur #fyp!'
+# TODO: Refactor this with subject, before, context, let, specify,
 
-  it 'should returning Array' do
-    expect(hashtags(simple_string).is_a?(Array)).to eq true
-  end
+describe HashTag do
+  describe '#has_any_hashtag' do
+    subject { hashtag }
+    # before { hashtag.has_any_hashtag? }
+    # after { hashtag.has_any_hashtag? }
 
-  it 'should grab one #tag' do
-    expect(hashtags(one_tag_string).size).to eq 1
-  end
+    context 'main context' do
+      let(:hashtag) { HashTag.new }
+      let!(:string) do
+        'Lorem ipsum dolor sit amet, consectetur #anime #аниме sit amet,
+   consectetur #fyp! #otaku #weeb? adipisicing elit, sed do eiusmod
+   tempor #f-mab #fullmetal_alchemist'
+      end
 
-  it 'should should grab several #tags' do
-    expect(hashtags(string).size).to be > 1
-  end
+      let!(:simple_string) do
+        'Lorem ipsum dolor sit amet'
+      end
 
-  it 'should should grab #tag rus' do
-    expect(hashtags(rus_tag_string).size).to eq 1
-  end
+      let!(:one_tag_string) do
+        'Lorem ipsum dolor sit amet, consectetur #anime'
+      end
 
-  it 'should should grab #ta_g with snake_case' do
-    expect(hashtags(snake_case_tag_string).size).to eq 1
-  end
+      let!(:rus_tag_string) do
+        'Lorem ipsum dolor sit amet, consectetur #аниме'
+      end
 
-  it 'should should grab #ta-g with minus' do
-    expect(hashtags(minus_tag_string).size).to eq 1
-  end
+      let!(:snake_case_tag_string) do
+        'Lorem ipsum adipisicing elit, #fullmetal_alchemist'
+      end
 
-  it 'should should grab #tag? with ?' do
-    expect(hashtags(question_tag_string).size).to eq 1
-  end
+      let!(:minus_tag_string) do
+        'Lorem ipsum dolor sit amet, consectetur #f-mab'
+      end
 
-  it 'should should grab #tag! with !' do
-    expect(hashtags(exclamation_tag_string).size).to eq 1
+      let!(:question_tag_string) do
+        'sit amet, consectetur #weeb?'
+      end
+
+      let!(:exclamation_tag_string) do
+        'consectetur sit amet, consectetur #fyp!'
+      end
+
+      specify { should_not have_any_hashtag }
+
+      it 'should returning Array' do
+        expect(hashtag.hashtags(simple_string).is_a?(Array)).to eq true
+        should_not have_any_hashtag
+      end
+
+      it 'should grab one #tag' do
+        expect(hashtag.hashtags(one_tag_string).size).to eq 1
+        should have_any_hashtag
+      end
+
+      it 'should should grab several #tags' do
+        expect(hashtag.hashtags(string).size).to be > 1
+      end
+
+      it 'should should grab #tag rus' do
+        expect(hashtag.hashtags(rus_tag_string).size).to eq 1
+      end
+
+      it 'should should grab #ta_g with snake_case' do
+        expect(hashtag.hashtags(snake_case_tag_string).size).to eq 1
+      end
+
+      it 'should should grab #ta-g with minus' do
+        expect(hashtag.hashtags(minus_tag_string).size).to eq 1
+      end
+
+      it 'should should grab #tag? with ?' do
+        expect(hashtag.hashtags(question_tag_string).size).to eq 1
+      end
+
+      it 'should should grab #tag! with !' do
+        expect(hashtag.hashtags(exclamation_tag_string).size).to eq 1
+      end
+    end
   end
 end
